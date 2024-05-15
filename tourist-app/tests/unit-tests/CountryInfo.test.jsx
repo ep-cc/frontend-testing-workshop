7b160c56d2b7e2111f8660922a1f995ba9a1aeec
Peter Erben <erben.peter@gmail.com>
2024-05-15 21:08:15 +0200

    add stateless test, check props

---
+++ tourist-app/tests/unit-tests/CountryInfo.test.jsx
@@ -0,0 +1,33 @@
+import { test, expect, describe } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import { render, screen } from '@testing-library/react';
+import CountryInfo from '../../src/CountryInfo';
+
+
+describe('CountryInfo component', () => {
+  
+  test('displays the proper values', () => {
+    const countryData = {
+      'name': {
+          'common': 'Sweden',
+          'official': 'Kingdom of Sweden',
+        },
+        'cca3': 'SWE',
+        'currencies': {
+          'SEK': {
+            'name': 'Swedish krona',
+            'symbol': 'kr'
+          }
+        },
+        'capitals': [
+          'Stockholm'
+        ],
+        'region': 'Europe',
+        'subregion': 'Northern Europe',
+    };
+    render(<CountryInfo countryData={countryData}/>);
+    expect(screen.getByText('Stockholm', {exact: false})).toBeInTheDocument();
+    expect(screen.getByText('Swedish krona', {exact: false})).toBeInTheDocument();
+  });
+
+});
