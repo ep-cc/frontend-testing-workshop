be1f4baeaaef5200a4c208d31f22d2e8da619dac
ep-cc <peter.erben@codecool.com>
2024-05-17 20:31:21 +0200

    second slide deck is complete

--- tourist-app/tests/unit-tests/CountryInfo.test.jsx
+++ tourist-app/tests/unit-tests/CountryInfo.test.jsx
@@ -1,11 +1,12 @@
 import { test, expect, describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
+// App related import
 import CountryInfo from '../../src/CountryInfo';
 
 
-describe('CountryInfo component', () => {
-  
+describe('CountryInfo component', () => {  
   test('displays the proper values', () => {
+    // arrange
     const countryData = {
       'name': {
           'common': 'Sweden',
@@ -24,9 +25,10 @@ describe('CountryInfo component', () => {
         'region': 'Europe',
         'subregion': 'Northern Europe',
     };
+    // act
     render(<CountryInfo countryData={countryData}/>);
+    //assert
     expect(screen.getByText('Stockholm', {exact: false})).toBeInTheDocument();
     expect(screen.getByText('Swedish krona', {exact: false})).toBeInTheDocument();
   });
-
 });
